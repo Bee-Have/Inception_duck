6aabed16f1c10bb878c39e5fbb829633789602c6
amarini- <duck.marini@gmail.com>
2023-01-31 16:54:36 +0100

    🚚 -Continuing to change the name of the env variable MYSQL_ROOT_PASSWORD

--- srcs/requirements/mariadb/mysql_setup.sh
+++ srcs/requirements/mariadb/mysql_setup.sh
@@ -22,17 +22,17 @@ DELETE FROM mysql.user WHERE User='';
 DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASSWORD}';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 CREATE DATABASE ${MYSQL_DATABASE};
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
-	mysql -u root -p$ROOT_PASSWORD < tmpfile
+	mysql -u root -p$MYSQL_ROOT_PASSWORD < tmpfile
 
 	sleep 4
 
-	mysqladmin -u root -p"$ROOT_PASSWORD" shutdown
+	mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
 	touch ${MYSQL_PATH}/mysql_initialized
 	rm -f tmpfile
 fi
