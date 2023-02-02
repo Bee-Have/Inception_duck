2ccb4883ece4aa794b49288500f10dbc03d351f6
amarini- <duck.marini@gmail.com>
2023-02-02 19:51:47 +0100

    🚧 -Trying to debug wordpress regarding "wp-config.php" which does not exists and is not created. (there's also a pb with wp --allow-root core download)

--- srcs/requirements/mariadb/mysql_setup.sh
+++ srcs/requirements/mariadb/mysql_setup.sh
@@ -1,16 +1,19 @@
+#!/bin/bash
+
 if [ ! -f ${MYSQL_PATH}/mysql_initialized ]; then
 
 	mysql_install_db --user=mysql --datadir=${MYSQL_PATH} --basedir=/usr --rpm > /dev/null
 	mysqld_safe &
 
-	MYSQL_UP=0
-	while [ $MYSQL_UP -eq 0 ]; do
-		sleep 5
-		ps aux | grep -v "grep" | grep "mysqld_safe"
-		if [ $? -eq 0 ]; then
-			MYSQL_UP=1
-		fi
-	done
+	sleep 2
+	# MYSQL_UP=0
+	# while [ $MYSQL_UP -eq 0 ]; do
+	# 	sleep 5
+	# 	ps aux | grep -v "grep" | grep "mysqld_safe"
+	# 	if [ $? -eq 0 ]; then
+	# 		MYSQL_UP=1
+	# 	fi
+	# done
 
 	touch tmpfile
 	chmod 777 tmpfile
@@ -22,7 +25,7 @@ DELETE FROM mysql.user WHERE User='';
 DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+ALTER USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 CREATE DATABASE ${MYSQL_DATABASE};
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%';
@@ -30,7 +33,7 @@ FLUSH PRIVILEGES;
 EOF
 	mysql -u root -p$MYSQL_ROOT_PASSWORD < tmpfile
 
-	sleep 4
+	sleep 2
 
 	mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
 	touch ${MYSQL_PATH}/mysql_initialized
@@ -39,4 +42,5 @@ fi
 
 sleep 1
 
+# service mysql start
 exec /usr/sbin/mysqld --user=root
