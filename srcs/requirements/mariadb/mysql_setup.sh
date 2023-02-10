738d483e8ba5d2e24370213ed07d899955d1bcd5
ldutriez-Inspiron <ldutriez@student.42.fr>
2023-02-10 17:03:09 +0100

    🐛 -Might have fixed mariadb issues MAYBE

--- srcs/requirements/mariadb/mysql_setup.sh
+++ srcs/requirements/mariadb/mysql_setup.sh
@@ -1,38 +1,54 @@
 #!/bin/bash
 
-if [ ! -f ${MYSQL_PATH}/mysql_initialized ]; then
+# if [ ! -f ${MYSQL_PATH}/mysql_initialized ]; then
 
-	mysql_install_db --user=root --datadir=${MYSQL_PATH} --basedir=/usr --rpm > /dev/null
-	mysqld_safe &
+	# mysql_install_db --user=root --datadir=${MYSQL_PATH} --basedir=/usr --rpm > /dev/null
+	# mysqld_safe &
 
-	sleep 2
+	# sleep 2
 
-	touch tmpfile
-	chmod 777 tmpfile
+	# touch tmpfile
+	# chmod 777 tmpfile
 
-	cat > tmpfile << EOF
-USE mysql;
-FLUSH PRIVILEGES;
-DELETE FROM mysql.user WHERE User='';
-DROP DATABASE IF EXISTS test;
-DELETE FROM mysql.db WHERE Db='test';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-ALTER USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%';
-FLUSH PRIVILEGES;
-EOF
-	mysql -u root -p$MYSQL_ROOT_PASSWORD < tmpfile
+	# cat > tmpfile << EOF
+	# mysql -u root -p$MYSQL_ROOT_PASSWORD -e "USE mysql;"
+	# mysql -u root -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
+	# mysql -u root -p$MYSQL_ROOT_PASSWORD -e "DELETE FROM mysql.user WHERE User='';"
+	# mysql -u root -p$MYSQL_ROOT_PASSWORD -e "DROP DATABASE IF EXISTS test;"
+	# mysql -u root -p$MYSQL_ROOT_PASSWORD -e "DELETE FROM mysql.db WHERE Db='test';"
+	# mysql -u root -p$MYSQL_ROOT_PASSWORD -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+	# mysql -u root -p$MYSQL_ROOT_PASSWORD -e "ALTER USER IF EXISTS 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+	# mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+	# mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+	# mysql -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%';"
+	# mysql -u root -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
+# EOF
+	# mysql -u root -p$MYSQL_ROOT_PASSWORD < tmpfile
 
-	sleep 2
+	# sleep 2
 
-	mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
-	touch ${MYSQL_PATH}/mysql_initialized
-	rm -f tmpfile
-fi
+	# mysqladmin -uroot -p"$MYSQL_ROOT_PASSWORD" shutdown
+	# touch ${MYSQL_PATH}/mysql_initialized
+	# rm -f tmpfile
+# fi
 
-sleep 1
+# sleep 1
+
+mysql_install_db --user=mysql --datadir=${MYSQL_PATH} --basedir=/usr --rpm > /dev/null
+mysqld_safe &
+
+sleep 2
+
+# mysql -u mysql -e "USE mysql;"
+# mysql -u mysql -e "FLUSH PRIVILEGES;"
+# mysql -u mysql -e "DELETE FROM mysql.user WHERE User='';"
+mysql -u root -e "CREATE DATABASE ${MYSQL_DATABASE};"
+mysql -u root -e "CREATE USER '${MYSQL_USER}'@'localhost';"
+mysql -u root -e "SET password FOR '${MYSQL_USER}'@'localhost' = password('${MYSQL_PASSWORD}');"
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -u root -e "FLUSH PRIVILEGES;"
+
+mysqladmin -uroot shutdown
 
 # service mysql restart
-exec /usr/sbin/mysqld --datadir=${MYSQL_PATH} --user=root
+exec /usr/sbin/mysqld --user=root
