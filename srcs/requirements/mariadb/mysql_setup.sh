94e7d1510208bf2e6f9e4f2263f029ef42bce08c
ldutriez-Inspiron <ldutriez@student.42.fr>
2023-02-24 19:32:25 +0100

    🔥🩹 -Deleted all comments in the mariadb directory

--- srcs/requirements/mariadb/mysql_setup.sh
+++ srcs/requirements/mariadb/mysql_setup.sh
@@ -1,39 +1,5 @@
 #!/bin/bash
 
-# if [ ! -f ${MYSQL_PATH}/mysql_initialized ]; then
-
-	# mysql_install_db --user=root --datadir=${MYSQL_PATH} --basedir=/usr --rpm > /dev/null
-	# mysqld_safe &
-
-	# sleep 2
-
-	# touch tmpfile
-	# chmod 777 tmpfile
-
-	# cat > tmpfile << EOF
-	# mysql -u root -p$MYSQL_ROOT_PASSWORD -e "USE mysql;"
-	# mysql -u root -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
-	# mysql -u root -p$MYSQL_ROOT_PASSWORD -e "DELETE FROM mysql.user WHERE User='';"
-	# mysql -u root -p$MYSQL_ROOT_PASSWORD -e "DROP DATABASE IF EXISTS test;"
-	# mysql -u root -p$MYSQL_ROOT_PASSWORD -e "DELETE FROM mysql.db WHERE Db='test';"
-	# mysql -u root -p$MYSQL_ROOT_PASSWORD -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
-	# mysql -u root -p$MYSQL_ROOT_PASSWORD -e "ALTER USER IF EXISTS 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-	# mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
-	# mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS '${MYSQL_ADMIN_USER}'@'%' IDENTIFIED BY '${MYSQL_ADMIN_PASSWORD}';"
-	# mysql -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_ADMIN_USER}'@'%';"
-	# mysql -u root -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
-# EOF
-	# mysql -u root -p$MYSQL_ROOT_PASSWORD < tmpfile
-
-	# sleep 2
-
-	# mysqladmin -uroot -p"$MYSQL_ROOT_PASSWORD" shutdown
-	# touch ${MYSQL_PATH}/mysql_initialized
-	# rm -f tmpfile
-# fi
-
-# sleep 1
-
 mysql_install_db --user=mysql --datadir=${MYSQL_PATH} --basedir=/usr --rpm > /dev/null
 mysqld_safe &
 
@@ -52,5 +18,4 @@ mysql -u root -e "FLUSH PRIVILEGES;"
 mysqlcheck --repair --all-databases
 mysqladmin -uroot shutdown
 
-# service mysql restart
 exec /usr/sbin/mysqld --datadir=${MYSQL_PATH} --user=root
