00142ba4eebaaa4dabfa15df92cb7a2cf029bf29
ldutriez-Inspiron <ldutriez@student.42.fr>
2023-02-24 19:50:18 +0100

    🔥🩹 -Deleted all comments from wordpress directory (except thow fron the .conf files)

--- srcs/requirements/wordpress/wp_start.sh
+++ srcs/requirements/wordpress/wp_start.sh
@@ -1,30 +1,6 @@
 #!/bin/bash
 
-# wp --path=${WP_PATH} --allow-root core is-installed
-# if [ $? -ne 0 ]
-# then
-# wp --path=${WP_PATH} --allow-root core download
-# 	# there is no config.php in the project. this line create such file and directly put it in the right place
-# 	wp config create --path=${WP_PATH} --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_ADMIN_USER} --dbpass=${MYSQL_ADMIN_PASSWORD} --dbhost=${DB_HOST} --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
-# 	# setup the website information for wordpress in the database
-# 	wp --allow-root core install --path=${WP_PATH} --url=${MY_URL} --title="${TITLE}" --admin_user=${MYSQL_ADMIN_USER} --admin_password=${MYSQL_ADMIN_PASSWORD} --admin_email=amarini-@student.42.fr
-# 	# create user ogenser hehe oscar ma boy
-# 	wp --path=${WP_PATH} --allow-root user create ogenser ogenser@student.42.fr --role=author --user_pass=ogenser
-# 	# create user lbetranc leti <3
-# 	wp --path=${WP_PATH} --allow-root user create lbetranc lbetranc@student.42.fr --role=author --user_pass=lbetranc
-# fi
-
-# cp -R /wordpress /var/www/html/
-
-# sleep 20
-
-# /usr/sbin/php-fpm7.3 -F
-
-# sleep 5
-
 wp core download --path=/var/www --allow-root
 wp config create --path=/var/www --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_ADMIN_USER --dbpass=$MYSQL_ADMIN_PASSWORD --dbhost=$DB_HOST --dbcharset=utf8 --allow-root --skip-check
-# cp /tmp/wp-config.php /var/www/wp-config.php
-# wp core install --path=/var/www --url=$URL --title=$TITLE --admin_name=$MYSQL_ADMIN_USER --admin_password=$MYSQL_ADMIN_PASSWORD --admin_email=$MYSQL_ADMIN_EMAIL --allow-root --skip-email
 
 /usr/sbin/php-fpm7.3 -F -R
